b5e6f5d5f7739127c0b3780df3bd61299a7ed630
nisann97 <nisann@code.edu.az>
2024-05-18 08:05:06 +0400

    added hw

---
+++ test/ViewModels/Blogs/BlogCreateVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace test.ViewModels.Blogs
+{
+	public class BlogCreateVM
+	{
+        [Required(ErrorMessage = "This input cannot be empty")]
+        [StringLength(20, ErrorMessage = "Cannot be more than 20")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "This input cannot be empty")]
+        [StringLength(20, ErrorMessage = "Cannot be more than 50")]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
+
